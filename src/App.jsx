26da4d089503af20ade3c362989840d2c10f2396
Lyrismet <ana0203tir@gmail.com>
2024-01-08 08:32:40 +0300

    style: prettier applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import './App.scss'
-import * as data from './data/backwords.json'
+import "./App.scss";
+import * as data from "./data/backwords.json";
 function App() {
+  const words = data.words;
+    const starCount = 80;
 
-    const words = data.words;
+    const createStars = () => {
+        const stars = [];
+        for (let i = 1; i <= starCount; i++) {
+            stars.push(<div className={`star star${i}`} key={i}></div>);
+        }
+        return stars;
+    };
+
   return (
     <main>
-        <div className="background-words">
-          {words.map((word, i) => (
-            <p className="back-word" key={i}>{word}</p>
-          ))}
-        </div>
+      <div className="background-words">
+        {words.map((word, i) => (
+          <p className="back-word" key={i}>
+            {word}
+          </p>
+        ))}
+      </div>
+      <div className="background-stars">{createStars()}</div>
       <h1>Hello World</h1>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_KEY = 'YnC1xVug0kvxp9isY5lRGOEV7coiIcCLfRJHa8EJ';
+const BASE_URL = 'https://api.nasa.gov/insight_weather/'
+
+export const getWeather = async () => {
+    try{
+        const response = await axios.get(`${BASE_URL}?api_key=${API_KEY}&feedtype=json&ver=1.0`);
+        return response.data;
+    } catch (error){
+        throw new Error('Failed fetch data')
+    }
+}
